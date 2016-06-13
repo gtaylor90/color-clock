3d0f13f0435a7967b472e13b7ac82e5ba01eede5
Graham Taylor <Graham@Grahams-MacBook-Pro.local>
2016-06-13 03:54:57 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var startTime = function() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+
+var checkTime = function(i) {
+    if (i < 10) {i = "0" + i}; 
+    return i;
+}
